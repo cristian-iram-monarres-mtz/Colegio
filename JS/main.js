fe29316ab8988c43903fd164d1b081e9cc5b67b4
Manuel_Saucedo <Manuel_Saucedo@hotmail.com>
2022-01-18 01:16:25 -0600

    Se realizó la página de inicio

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+
+ScrollReveal({
+  reset: true,
+  distance: '110px',
+  duration: 2500,
+  delay: 400
+});
+//buttom
+ScrollReveal().reveal('.btn-outline-warning', { delay: 300 });
+//teacher
+ScrollReveal().reveal('.container-teacher', { delay: 300, origin: 'left' });
+//contact
+ScrollReveal().reveal('.container-contact', { delay: 300, origin: 'rigth' });
+//location
+ScrollReveal().reveal('.container-location', { delay: 300, origin: 'bottom' });
+
